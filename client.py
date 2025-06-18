6b9b71588c90a37e1068848f0c6b5906e8f85d5d
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-06-18 14:52:53 -0300

    mensajes encriptados

--- client.py
+++ client.py
@@ -1,12 +1,48 @@
 import socket
 import time
 import logging
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 HOST = "192.168.0.10"
 PORT = 12345
 NUM_PAQUETES = 10
 INTERVALO_SEGUNDOS = 1
 LOG_FILE = "client.log"
+
+
+# Obtener session_key y enc_session_key a partir de public_key del servidor
+def cifrar_clave_sesion_con_rsa(public_pem):
+    public_key      = RSA.import_key(public_pem)  # ahora es RsaKey
+    cipher_rsa      = PKCS1_OAEP.new(public_key)
+    session_key     = get_random_bytes(16)  # AES-128
+    enc_session_key = cipher_rsa.encrypt(session_key)
+    return session_key, enc_session_key
+
+# Cifrado y descifrado de mensajes 
+def cifrar_payload(payload_bytes, session_key):
+    """
+    Cifra payload_bytes con AES-CTR. Devuelve nonce y ciphertext.
+    """
+    # AES.new con MODE_CTR genera internamente un contador basado en nonce
+    nonce = get_random_bytes(8)  # fijo 16 bytes
+    cipher = AES.new(session_key, AES.MODE_CTR, nonce=nonce)
+    ciphertext = cipher.encrypt(payload_bytes)
+    
+    # nonce es necesario para descifrar
+    return nonce, ciphertext
+
+def descifrar_payload(nonce, ciphertext, session_key):
+    """
+    Descifra ciphertext con AES-CTR usando nonce y session_key.
+    """
+    cipher = AES.new(session_key, AES.MODE_CTR, nonce=nonce)
+    datos = cipher.decrypt(ciphertext)
+    return datos
+
+
 
 def configurar_logging():
     logging.basicConfig(
@@ -24,6 +60,13 @@ def main():
         try:
             s.connect((HOST, PORT))
             logging.info(f"Conectado al servidor {HOST}:{PORT}")
+            data = s.recv(1024)
+            logging.info(f"Clave pública recibida: {len(data)} bytes")
+            session_key, enc_session_key = cifrar_clave_sesion_con_rsa(data)
+            logging.info(f"Session key generada: {session_key.hex()}, ciphertext: {len(enc_session_key)} bytes")
+            s.sendall(enc_session_key)
+            logging.info(f"Sesion key {session_key.hex()}")
+
         except Exception as e:
             logging.error(f"No se pudo conectar al servidor: {e}")
             return
@@ -34,7 +77,8 @@ def main():
             mensaje = f"{texto_previo} {num}"
             logging.info(f"Enviando: {mensaje}")
             try:
-                s.sendall(mensaje.encode('utf-8'))
+                nonce, ciphertext = cifrar_payload(mensaje.encode('utf-8'),session_key)
+                s.sendall(nonce + ciphertext)
             except Exception as e:
                 logging.error(f"Error al enviar paquete: {e}")
                 break
@@ -48,12 +92,12 @@ def main():
             if not data:
                 logging.info("El servidor cerró la conexión inesperadamente.")
                 break
-            try:
-                texto = data.decode('utf-8').strip()
-            except UnicodeDecodeError:
-                logging.info("Respuesta recibida no es UTF-8, ignorando.")
-                continue
+            
+            nonce      = data[:8]
+            ciphertext = data[8:]
+            data       = descifrar_payload(nonce,ciphertext,session_key)
 
+            texto = data.decode('utf-8').strip()
             logging.info(f"Recibido del servidor: {texto}")
 
             if ' ' in texto:
--- server.py
+++ server.py
@@ -1,9 +1,62 @@
 import socket
 import logging
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 HOST = "0.0.0.0"
 PORT = 12345
 LOG_FILE = "server.log"
+
+
+
+# Generar claves publicas y privadas
+def generar_claves_rsa(ruta_privada="private.pem", ruta_publica="public.pem", tamaño=2048):
+    key = RSA.generate(tamaño)
+    private_key = key.export_key()
+    public_key  = key.publickey().export_key()
+    # Guardar en archivos
+    with open(ruta_privada, "wb") as f:
+        f.write(private_key)
+    with open(ruta_publica, "wb") as f:
+        f.write(public_key)
+    print(f"Claves RSA generadas:\n - Privada: {ruta_privada}\n - Pública: {ruta_publica}")
+
+def get_public_key(ruta_publica="public.pem"):
+    with open(ruta_publica, "rb") as f:
+        return f.read()  # bytes PEM
+
+# Función para descifrar la clave de sesión en el servidor usando RSA privada
+def descifrar_clave_sesion_con_rsa(enc_session_key, ruta_privada="private.pem"):
+    with open(ruta_privada, "rb") as f:
+        private_pem = f.read()  # bytes PEM
+    private_key     = RSA.import_key(private_pem)  # ahora es RsaKey
+    cipher_rsa      = PKCS1_OAEP.new(private_key)
+    session_key     = cipher_rsa.decrypt(enc_session_key)
+    return session_key
+
+# Cifrado y descifrado de mensajes 
+def cifrar_payload(payload_bytes, session_key):
+    """
+    Cifra payload_bytes con AES-CTR. Devuelve nonce y ciphertext.
+    """
+    # AES.new con MODE_CTR genera internamente un contador basado en nonce
+    nonce = get_random_bytes(8)  # fijo 16 bytes
+    cipher = AES.new(session_key, AES.MODE_CTR, nonce=nonce)
+    ciphertext = cipher.encrypt(payload_bytes)
+    
+    # nonce es necesario para descifrar
+    return nonce, ciphertext
+
+def descifrar_payload(nonce, ciphertext, session_key):
+    """
+    Descifra ciphertext con AES-CTR usando nonce y session_key.
+    """
+    cipher = AES.new(session_key, AES.MODE_CTR, nonce=nonce)
+    datos = cipher.decrypt(ciphertext)
+    return datos
+
 
 def configurar_logging():
     logging.basicConfig(
@@ -22,10 +75,21 @@ def run_server():
         s.listen()
         logging.info(f"Escuchando en {HOST}:{PORT}...")
         conn, addr = s.accept()
+
         with conn:
             logging.info(f"Conectado por {addr}")
+            public_key = get_public_key()
+            conn.sendall(public_key)
+            enc_session_key = conn.recv(256)
+            logging.info(f"Sesion key encriptada recibida {addr}")
+            session_key = descifrar_clave_sesion_con_rsa(enc_session_key)
+            logging.info(f"Sesion key {session_key.hex()}")
+
             while True:
-                data = conn.recv(1024)
+                data       = conn.recv(1024)
+                nonce      = data[:8]
+                ciphertext = data[8:]
+                data       = descifrar_payload(nonce,ciphertext,session_key)
                 if not data:
                     logging.info("Cliente desconectado, cerrando servidor.")
                     break
@@ -50,8 +114,11 @@ def run_server():
                     respuesta_texto = "ERROR: No es un entero válido"
                     respuesta_bytes = respuesta_texto.encode('utf-8')
 
-                conn.sendall(respuesta_bytes)
+                nonce, ciphertext = cifrar_payload(respuesta_bytes,session_key)
+                conn.sendall(nonce + ciphertext)
+
                 logging.info(f"Enviado a {addr}: {respuesta_texto}")
 
 if __name__ == "__main__":
+    generar_claves_rsa()
     run_server()
